ffb97ac63ab8f1c49b308eab733d8b46b1eba188
Oscar Veldman <oveldman@gmail.com>
2025-03-20 15:58:48 +0100

    Update example project with an example

--- src/MadWorldNL.EarCut.Console/Program.cs
+++ src/MadWorldNL.EarCut.Console/Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using MadWorldNL.EarCut.Logic;
+
+var triangles = EarCut.Calculate([0, 0, 0, 50, 50, 00], null, 2);
+Console.Write("triangles: " + string.Join(", ", triangles));
+
+Console.WriteLine("This is a simple example of using the EarCut algorithm in C#.");
