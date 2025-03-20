1f811bcec8df48ef486399761dd4b8c9ea97f7fc
Oscar Veldman <oveldman@gmail.com>
2025-03-20 23:46:13 +0100

    Add comment

--- src/MadWorldNL.EarCut.Logic/EarCut.cs
+++ src/MadWorldNL.EarCut.Logic/EarCut.cs
@@ -7,11 +7,25 @@ namespace MadWorldNL.EarCut.Logic;
 
 public static class EarCut
 {
+    /// <summary>
+    /// Triangulates the given polygon
+    /// </summary>
+    /// <param name="data">is a flat array of vertex coordinates like [x0,y0, x1,y1, x2,y2, ...].</param>
+    /// <param name="holeIndices">is an array of hole indices if any (e.g. [5, 8] for a 12-vertex input would mean one hole with vertices 5-7 and another with 8-11).</param>
+    /// <param name="dim">is the number of coordinates per vertex in the input array</param>
+    /// <returns>List containing groups of three vertex indices in the resulting array forms a triangle.</returns>
     public static List<int> Calculate(double[]? data, int[]? holeIndices = null, int dim = 2)
     {
         return EarCut<double>.Calculate(data, holeIndices, dim);
     }
     
+    /// <summary>
+    /// Triangulates the given polygon
+    /// </summary>
+    /// <param name="data">is a flat array of vertex coordinates like [x0,y0, x1,y1, x2,y2, ...].</param>
+    /// <param name="holeIndices">is an array of hole indices if any (e.g. [5, 8] for a 12-vertex input would mean one hole with vertices 5-7 and another with 8-11).</param>
+    /// <param name="dim">is the number of coordinates per vertex in the input array</param>
+    /// <returns>List containing groups of three vertex indices in the resulting array forms a triangle.</returns>
     public static List<int> Calculate(float[]? data, int[]? holeIndices = null, int dim = 2)
     {
         return EarCut<float>.Calculate(data, holeIndices, dim);
