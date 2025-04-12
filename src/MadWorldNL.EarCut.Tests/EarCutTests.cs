c0017cf1c3d47974228052993ebd11520adc0a93
Oscar Veldman <oveldman@gmail.com>
2025-04-12 07:55:29 +0200

    Add deviation to verify the completeness of the triangulation (#9)

--- src/MadWorldNL.EarCut.Tests/EarCutTests.cs
+++ src/MadWorldNL.EarCut.Tests/EarCutTests.cs
@@ -5,27 +5,27 @@ public class EarCutTests
     [Fact]
     public void SimpleTriangleDouble()
     {
-        var triangles = Logic.EarCut<double>.Tessellate([0, 0, 0, 50, 50, 00], null, 2);
+        var triangles = Logic.EarCut<double>.Tessellate([0, 0, 0, 50, 50, 00]);
         Assert.Equal([1, 0, 2], triangles);
     }
     
     [Fact]
     public void SimpleTriangleFloat()
     {
-        var triangles = Logic.EarCut<float>.Tessellate([0, 0, 0, 50, 50, 00], null, 2);
+        var triangles = Logic.EarCut<float>.Tessellate([0, 0, 0, 50, 50, 00]);
         Assert.Equal([1, 0, 2], triangles);
     }
     
     [Fact]
     public void NotSoSimpleTriangleDouble() {
-        var triangles = Logic.EarCut<double>.Tessellate([0, 0, 0, 25, 0, 50, 25, 25, 50, 0, 25, 0], null, 2);
+        var triangles = Logic.EarCut<double>.Tessellate([0, 0, 0, 25, 0, 50, 25, 25, 50, 0, 25, 0]);
         // Not optimal, but correct.
         Assert.Equal([ 1, 0, 5, 5, 4, 3, 3, 2, 1, 1, 5, 3 ], triangles);
     }
     
     [Fact]
     public void NotSoSimpleTriangleFloat() {
-        var triangles = Logic.EarCut<float>.Tessellate([0, 0, 0, 25, 0, 50, 25, 25, 50, 0, 25, 0], null, 2);
+        var triangles = Logic.EarCut<float>.Tessellate([0, 0, 0, 25, 0, 50, 25, 25, 50, 0, 25, 0]);
         // Not optimal, but correct.
         Assert.Equal([ 1, 0, 5, 5, 4, 3, 3, 2, 1, 1, 5, 3 ], triangles);
     }
@@ -33,28 +33,28 @@ public class EarCutTests
     [Fact]
     public void LShapeDouble()
     {
-        var triangles = Logic.EarCut<double>.Tessellate([0, 0, 10, 0, 10, 5, 5, 5, 5, 15, 0, 15], null, 2);
+        var triangles = Logic.EarCut<double>.Tessellate([0, 0, 10, 0, 10, 5, 5, 5, 5, 15, 0, 15]);
         Assert.Equal([4, 5, 0, 0, 1, 2, 3, 4, 0, 0, 2, 3], triangles);
     }
     
     [Fact]
     public void LShapeFloat()
     {
-        var triangles = Logic.EarCut<float>.Tessellate([0, 0, 10, 0, 10, 5, 5, 5, 5, 15, 0, 15], null, 2);
+        var triangles = Logic.EarCut<float>.Tessellate([0, 0, 10, 0, 10, 5, 5, 5, 5, 15, 0, 15]);
         Assert.Equal([4, 5, 0, 0, 1, 2, 3, 4, 0, 0, 2, 3], triangles);
     }
 
     [Fact]
     public void SimplePolygonDouble()
     {
-        var triangles = Logic.EarCut<double>.Tessellate([10, 0, 0, 50, 60, 60, 70, 10], null, 2);
+        var triangles = Logic.EarCut<double>.Tessellate([10, 0, 0, 50, 60, 60, 70, 10]);
         Assert.Equal([1, 0, 3, 3, 2, 1], triangles);
     }
     
     [Fact]
     public void SimplePolygonFloat()
     {
-        var triangles = Logic.EarCut<float>.Tessellate([10, 0, 0, 50, 60, 60, 70, 10], null, 2);
+        var triangles = Logic.EarCut<float>.Tessellate([10, 0, 0, 50, 60, 60, 70, 10]);
         Assert.Equal([1, 0, 3, 3, 2, 1], triangles);
     }
 
@@ -62,7 +62,7 @@ public class EarCutTests
     public void PolygonWithHoleDouble()
     {
         var triangles = Logic.EarCut<double>.Tessellate(
-            [0, 0, 100, 0, 100, 100, 0, 100, 20, 20, 80, 20, 80, 80, 20, 80], [4], 2);
+            [0, 0, 100, 0, 100, 100, 0, 100, 20, 20, 80, 20, 80, 80, 20, 80], [4]);
         Assert.Equal([ 3, 0, 4, 5, 4, 0, 3, 4, 7, 5, 0, 1, 2, 3, 7, 6, 5, 1, 2, 7, 6, 6, 1, 2], triangles);
     }
     
@@ -70,7 +70,7 @@ public class EarCutTests
     public void PolygonWithHoleFloat()
     {
         var triangles = Logic.EarCut<float>.Tessellate(
-            [0, 0, 100, 0, 100, 100, 0, 100, 20, 20, 80, 20, 80, 80, 20, 80], [4], 2);
+            [0, 0, 100, 0, 100, 100, 0, 100, 20, 20, 80, 20, 80, 80, 20, 80], [4]);
         Assert.Equal([ 3, 0, 4, 5, 4, 0, 3, 4, 7, 5, 0, 1, 2, 3, 7, 6, 5, 1, 2, 7, 6, 6, 1, 2], triangles);
     }
 
@@ -90,5 +90,33 @@ public class EarCutTests
         List<int> triangles = Logic.EarCut<float>.Tessellate(polygon, null, 3);
         
         Assert.Equal([ 1, 0, 3, 3, 2, 1], triangles);
+    }
+    
+    [Fact]
+    public void Deviation_GivenWhenSimple2DPolygon_ThenReturnsCorrectDeviation()
+    {
+        // Arrange
+        double[] polygon = [0, 0, 0, 50, 50, 00];
+        List<int> triangles = [1, 0, 2];
+        
+        // Act
+        var percentage = Logic.EarCut.Deviation(polygon, triangles);
+
+        // Assert
+        Assert.Equal(0, percentage);
+    }
+    
+    [Fact]
+    public void Deviation_GivenWhenSimple2DPolygon_ThenReturnsMissedPercentage()
+    {
+        // Arrange
+        double[] polygon = [10, 0, 0, 50, 60, 60, 70, 10];
+        List<int> triangles = [3, 2, 1];
+        
+        // Act
+        var percentage = Logic.EarCut.Deviation(polygon, triangles);
+
+        // Assert
+        Assert.Equal(0.5, percentage);
     }
 }
